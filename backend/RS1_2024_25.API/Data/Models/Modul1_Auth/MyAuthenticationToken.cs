5179ab9a11417b9f61ea5dc07452c0ccf66b6ef6
Anesa <anesa.atlic@edu.fit.ba>
2024-12-21 12:46:51 +0100

    Add init backend

---
+++ backend/RS1_2024_25.API/Data/Models/Modul1_Auth/MyAuthenticationToken.cs
@@ -0,0 +1,21 @@
+﻿using RS1_2024_25.API.Helper.BaseClasses;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.Data.Models.Modul1_Auth;
+
+public class MyAuthenticationToken : TenantSpecificTable
+{
+    public required string Value { get; set; } // Token string
+
+    public string IpAddress { get; set; } = string.Empty;// IP address of the client
+
+    public DateTime RecordedAt { get; set; } // Timestamp of token creation
+
+    // Foreign key to link the token to a specific user
+    [ForeignKey(nameof(MyAppUser))]
+    public int MyAppUserId { get; set; }
+
+    public MyAppUser? MyAppUser { get; set; } // Navigation property to the user
+}
