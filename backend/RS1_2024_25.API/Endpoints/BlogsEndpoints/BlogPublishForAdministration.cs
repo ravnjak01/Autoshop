1bb3fae18137e3d281811f2e24f0a48cfffe8244
Anesa <anesa.atlic@edu.fit.ba>
2025-01-01 19:10:09 +0100

    Add publish action

---
+++ backend/RS1_2024_25.API/Endpoints/BlogsEndpoints/BlogPublishForAdministration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RS1_2024_25.API.Data.Models.Modul2_Basic;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Helper.Api;
+using Microsoft.EntityFrameworkCore;
+
+namespace RS1_2024_25.API.Endpoints.BlogsEndpoints
+{
+    [Route("publish-blog")]
+    public class BlogPublishForAdministration(ApplicationDbContext db) : MyEndpointBaseAsync
+    .WithRequest<int>
+    .WithoutResult
+    {
+
+        [HttpPost("{id}")]
+        public override async Task HandleAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var blog = await db.BlogPosts.SingleOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+            if (blog == null)
+                throw new KeyNotFoundException("Blog not found");
+            blog.IsPublished = !blog.IsPublished;
+
+            if (blog.IsPublished)
+            {
+                blog.PublishedDate = DateTime.Now;
+            }
+            else
+            {
+                blog.PublishedDate = null;
+            }
+
+            db.Set<BlogPost>().Attach(blog);
+            db.Entry(blog).Property("IsPublished").IsModified = true;
+            db.Entry(blog).Property("PublishedDate").IsModified = true;
+
+            await db.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
