2f20ceca7655f4efa96b00f557b6e312e23036ae
Anesa <anesa.atlic@edu.fit.ba>
2025-01-08 20:24:56 +0100

    Implement Audit log

---
+++ backend/RS1_2024_25.API/Data/Models/Modul3_Audit/AuditLog.cs
@@ -0,0 +1,12 @@
+﻿namespace RS1_2024_25.API.Data.Models.Modul3_Audit
+{
+    public class AuditLog
+    {
+            public int Id { get; set; }
+            public string UserEmail { get; set; }
+            public required string EntityName { get; set; }
+            public required string Action { get; set; }
+            public DateTime Timestamp { get; set; }
+            public required string Changes { get; set; }
+    }
+}
