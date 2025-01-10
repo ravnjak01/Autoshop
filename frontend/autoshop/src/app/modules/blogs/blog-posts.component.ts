08f4418a64c6e22de85350148e5bb25756f3fa39
Anesa <anesa.atlic@edu.fit.ba>
2025-01-10 21:38:13 +0100

    Add searching field and search by title, content and author

--- frontend/autoshop/src/app/modules/blogs/blog-posts.component.ts
+++ frontend/autoshop/src/app/modules/blogs/blog-posts.component.ts
@@ -20,7 +20,7 @@ export class BlogListComponent implements OnInit {
 
   scrollDistance = 2; // Trigger the event a little later
   scrollUpDistance = 3; // Same for upward scrolling
-
+  searchQuery = '';
 
   constructor(
     private blogService: BlogsGetAllService,
@@ -34,7 +34,8 @@ export class BlogListComponent implements OnInit {
   loadBlogs(): void {
     var request: BlogGetAllRequest = {
       pageNumber: this.currentPage,
-      pageSize: this.pageSize
+      pageSize: this.pageSize,
+      searchQuery: this.searchQuery
     }
     this.blogService.handleAsync(request).subscribe(response => {
       this.totalCount = response.totalCount;
@@ -57,5 +58,12 @@ export class BlogListComponent implements OnInit {
 
   openBlog(id: number) {
     this.router.navigate(['/blog', id]);
+  }
+
+  applyFilter($event: KeyboardEvent) {
+    this.searchQuery = ($event.target as HTMLInputElement).value; // Capture the search input
+    this.currentPage = 1; // Reset the page number for new search
+    this.blogs = []; // Clear the previous blogs
+    this.loadBlogs();
   }
 }
