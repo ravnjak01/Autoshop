afc3cf024f68e0e1ac2245e6fadca70cb6faf88e
Muharem Ravnjak <muharem.ravnjak@edu.fit.ba>
2025-05-26 14:17:36 +0200

    BE:Added  files  for forgot-password functionality #16

---
+++ backend/RS1_2024_25.API/Services/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Options;
+using RS1_2024_25.API.Data.Models;
+using RS1_2024_25.API.Services;
+using System.Net;
+using System.Net.Mail;
+
+public class EmailService : IEmailService
+{
+    private readonly EmailSettings _emailSettings;
+
+    public EmailService(IOptions<EmailSettings> emailSettings)
+    {
+        _emailSettings = emailSettings.Value;
+    }
+
+  
+    public async Task SendResetPasswordEmail(string toEmail, string resetLink)
+    {
+        try
+        {
+            var mail = new MailMessage
+            {
+                From = new MailAddress(_emailSettings.From),
+                Subject = "Reset lozinke",
+                Body = $"Kliknite na link za reset lozinke: {resetLink}",
+                IsBodyHtml = false
+            };
+            mail.To.Add(toEmail);
+
+            using var smtp = new SmtpClient(_emailSettings.SmtpServer, _emailSettings.Port)
+            {
+                Credentials = new NetworkCredential(_emailSettings.Username, _emailSettings.Password),
+                EnableSsl = true
+            };
+
+            await smtp.SendMailAsync(mail);
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine("Email slanje nije uspjelo: " + ex.Message);
+            Console.WriteLine("Detalji greške: " + ex.ToString());
+            throw new InvalidOperationException("Greška pri slanju emaila", ex);
+            
+        }
+    }
+
+}
---
+++ backend/RS1_2024_25.API/Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace RS1_2024_25.API.Services
+{
+    public interface IEmailService
+    {
+        Task SendResetPasswordEmail(string toEmail, string resetLink);
+    }
+}
