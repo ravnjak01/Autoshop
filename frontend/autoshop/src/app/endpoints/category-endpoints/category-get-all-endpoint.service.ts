607f15a49b127d79e9df10f628306530337a6626
Anesa <anesa.atlic@edu.fit.ba>
2025-11-09 22:55:22 +0100

    Implement logic for connecting discount and categories

--- frontend/autoshop/src/app/endpoints/category-endpoints/category-get-all-endpoint.service.ts
+++ frontend/autoshop/src/app/endpoints/category-endpoints/category-get-all-endpoint.service.ts
@@ -1,12 +1,8 @@
 import {Category} from '../product-endpoints/product-get-all-endpoint.service';
 import {Injectable} from '@angular/core';
-import {MyBaseEndpointAsync, MyBaseEndpointAsyncWithoutRequest} from '../../helper/my-base-endpoint-async.interface';
+import {MyBaseEndpointAsyncWithoutRequest} from '../../helper/my-base-endpoint-async.interface';
 import {MyConfig} from '../../my-config';
 import {HttpClient} from '@angular/common/http';
-import {MyCacheService} from '../../services/cache-service/my-cache.service';
-import {of} from 'rxjs';
-import {buildHttpParams} from '../../helper/http-params.helper';
-import {tap} from 'rxjs/operators';
 
 export interface CategoryGetAllResponse {
   categories: Category[];
